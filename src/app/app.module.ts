5e0997afe389d425c355d379f4665fec246ccc5b
patrickcrocker <patrick.crocker@gmail.com>
2017-10-26 11:31:14 -0500

    use Staticfile with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: AppComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
